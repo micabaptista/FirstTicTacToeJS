165e75176e8a92037c403a9f0ac2ac16f490d400
micabaptista <baptistamichael47@gmail.com>
2021-01-20 16:18:46 +0000

    teste 1

--- src/index.js
+++ src/index.js
@@ -3,14 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Square(props){
-    return (
-        <button
-            className="square"
-            onClick={props.onClick}
-        >
-            {props.value}
-        </button>
-    );
+
 }
 
 class Board extends React.Component {
